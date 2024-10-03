6c413c34fd9be0b0a396842bdee8580e3bcb5342
soal-ec <165453551+soal-ec@users.noreply.github.com>
2024-10-03 09:06:40 +0800

    Add files via upload
    initial commit

---
+++ dailybean/home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, Review
+from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth import logout
+
+def homepage(request):
+    context = {
+        'is_authenticated': request.user.is_authenticated,
+        'products': Product.objects.all(),
+    }
+    return render(request, 'home.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('../../')
+
