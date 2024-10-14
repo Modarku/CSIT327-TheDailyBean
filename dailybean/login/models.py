c90099f5ee907fb7ee06426bdd56e3a884f2f161
Modarku <150920220+Modarku@users.noreply.github.com>
2024-10-14 09:51:06 +0800

    additional edits

--- dailybean/login/models.py
+++ dailybean/login/models.py
@@ -11,7 +11,7 @@ class UserManager(BaseUserManager):
 
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)  # Hash the password
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
