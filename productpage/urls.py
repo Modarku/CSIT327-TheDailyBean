82788492fe9753799282738a6187b4972d8de5dc
moriel <morielbien101@gmail.com>
2024-10-16 22:17:33 +0800

    product page

--- productpage/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.view_products, name='product_list'),
-    path('<int:id>/', views.product_detail, name='product_detail'),
-]
