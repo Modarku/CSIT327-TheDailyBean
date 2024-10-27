9730b14767b4aa251c5eecff48d9d9bb19388ca0
Modarku <150920220+Modarku@users.noreply.github.com>
2024-10-27 21:17:44 +0800

    checkpoint 3 begins

--- dailybean/accountsettings/views.py
+++ dailybean/accountsettings/views.py
@@ -16,9 +16,11 @@ def account_view(request):
                 return redirect('../../account/')
             else:
                 messages.error(request, form.errors, extra_tags='msg_edit_user') 
+        else:
+            form = UserEditForm(instance=user)
 
         # Handle password change
-        elif 'change_password' in request.POST:
+        if 'change_password' in request.POST:
             password_form = SetPasswordForm(user, request.POST)
             if password_form.is_valid():
                 password_form.save()
@@ -26,6 +28,8 @@ def account_view(request):
                 return redirect('../../login/')
             else:
                 messages.error(request, password_form.errors, extra_tags='msg_change_password') 
+        else:
+            password_form = SetPasswordForm(user)
     else:
         form = UserEditForm(instance=user)
         password_form = SetPasswordForm(user)
@@ -33,8 +37,8 @@ def account_view(request):
     context = {
         'is_authenticated': request.user.is_authenticated,
         'user': request.user,
-        'form': form,  # Make sure form is initialized here
-        'password_form': password_form,  # Pass password form to context
+        'form': form,
+        'password_form': password_form,
     }
 
     return render(request, 'accounts.html', context)
