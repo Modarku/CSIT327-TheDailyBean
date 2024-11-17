e88ecb8b3889aa4aa366acb2c57d22e41794086d
soal-ec <alecgkso@gmail.com>
2024-11-17 23:57:51 +0800

    AForge Video Library Added + UI cleanup

---
+++ dailybean/favorites/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from login.models import User
+from home.models import Product
+
+# Create your models here.
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'product') 
