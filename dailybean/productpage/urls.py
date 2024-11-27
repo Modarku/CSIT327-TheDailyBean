a8090dcd2aad709539312fcbc61385855156e3f5
Muy Bien <108384575+medab0@users.noreply.github.com>
2024-11-27 11:16:00 +0800

    phone number and delete account
    Add a phone number to the person entity and the user can delete their account

---
+++ dailybean/productpage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.view_products, name='product_list'),
+    path('<int:id>/', views.product_detail, name='product_detail'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('products/', views.search_products, name='search_products'),
+]
