4b95354a838cc7c06c71a9e020538c30a03f061b
Muy Bien <108384575+medab0@users.noreply.github.com>
2024-12-01 21:45:23 +0800

    Delete dailybean/cartlist directory

--- dailybean/cartlist/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.view_cart, name='cartpage'),
-    path('delete-order/<int:order_id>/', views.delete_order, name='delete_order'),
-]
