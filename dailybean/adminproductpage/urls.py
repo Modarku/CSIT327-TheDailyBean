0a97b284ecc5405bcd186896a1e7bfce1d72fab2
Muy Bien <108384575+medab0@users.noreply.github.com>
2024-12-01 21:45:10 +0800

    Delete dailybean/adminproductpage directory

--- dailybean/adminproductpage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.view_products, name='adminproductpage'),
-    path('add/', views.add_product, name='addproduct'),
-    path('modify/<int:pk>/', views.modify_product, name='modifyproduct'),
-]
